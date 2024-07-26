6eaa8f44e77f5b73cf24d008dfc6181ae19d0030
Sandhya Yadav <108703398+24267sandhya@users.noreply.github.com>
2024-07-26 11:16:14 +0530

    first commit

---
+++ mern-chat-app/backend/socket/socket.js
@@ -0,0 +1,54 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import { createClient } from "redis";
+import { createAdapter } from "@socket.io/redis-adapter";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
+});
+
+const pubClient = createClient({
+  password: "6dY03bKKjSX5OoDurIYhruygGTH5NqzN",
+  socket: {
+    host: "redis-12815.c44.us-east-1-2.ec2.redns.redis-cloud.com",
+    port: 12815,
+  },
+});
+
+const subClient = pubClient.duplicate();
+
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+  io.adapter(createAdapter(pubClient, subClient));
+});
+
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+const userSocketMap = {}; // {userId: socketId}
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId !== "undefined") userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // socket.on() is used to listen to the events. can be used both on client and server side
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { app, io, server };
